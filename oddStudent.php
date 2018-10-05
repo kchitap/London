43fc3f2e482c68f9b263180ce882d786929aee4b
kchitap <43220337+kchitap@users.noreply.github.com>
2018-10-05 08:29:21 -0400

    a

---
+++ oddStudent.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+	<head>
+		<meta charset="UTF-8">
+		<title>Lab 7: Odd One Out</title>
+	</head>
+
+	<body>
+
+		<header>
+
+			<h1>Political Science: Study and Practice</h1>
+
+		</header>
+
+		<nav>
+			<ul>
+				<li><a href="index.php">Media</a></li>
+				<li><a href="oddStudent.php">Politics</a></li>
+			</ul>
+		</nav>
+
+		<figure><img src="images/CityHall.jpeg" alt="A portrait of Rochester City Hall"></figure>
+
+		<section class="ContentBlock" id="KalebAbout">
+
+			<figure><img src="images/Kaleb.jpg" alt="A picture of Kaleb and his father"></figure>
+
+			<h2>Hometown</h2>
+			<p>I am from Rochester New York and attended high school in the city of Rochester. I have lived within 30 minutes of the city of Rochester for my entire life.</p>
+
+		</section>
+
+		<section class="ContentBlock" id="KalebPoliSci">
+
+			<figure><img src="images/Books.jpg" alt="A closup of books on a shelf, one by Thomas Jefferson is in the forefront"></figure>
+
+			<h2>Theoretical Politics</h2>
+			<p>My Political Science major has a focus on theoretical political science and the use of models in research. Using this discipline I plan on completing research in the field of voting institutions, outcomes, and logical choice in voting.</p>
+
+		</section>
+
+		<section class="ContentBlock" id="KalebCareer">
+
+			<figure><img src="images/Government.png" alt="A basic graphic of a city hall building dome inside a blue circle"></figure>
+
+			<h2>Working at Home</h2>
+			<p> For years I've wanted to work in government and public service in some way, hopefully in Rochester, my hometown.</p>
+
+		</section>
+
+	</body>
+
+</html>
