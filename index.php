7d775c8157e69dd29c7d33f731dfb0065b13d625
kchitap <43220337+kchitap@users.noreply.github.com>
2018-10-23 16:15:02 -0400

    completely updated the content

--- index.php
+++ index.php
@@ -3,123 +3,48 @@
 <html lang="en">
 
 	<head>
+
 		<meta charset="UTF-8">
-		<?php
-	  include('./inc/title.php');
-	  ?>
-		<link rel="stylesheet" href="css/index.css">
-	
+		<title>Odd Student Out</title>
+		<link type="text/css" rel="stylesheet" href="css/homepage.css">
 	</head>
 
 	<body>
-		<div="grid">
-		<header>
-			
-		<?php include('./inc/page-header.php');?>
 
-		<?php include('./inc/nav.php');?>
+		<section class="topLeft">
+			<header>
 
+				<h1>Odd Student Out: 3 students and their majors and jobs</h1>
 
-		</header>
+			</header>
+		</section>
+
+		<section class="topRight">
+			<nav>
+				<ul>
+					<li>
+						<a href="index.html">Home</a>
+						<a href="students.html">Student Info</a>
+					</li>
+				<ul>
+		</section>
+
+		<div class="center">
+			<img src="images/Odd.png">
+		</div>
+
+		<section class="bottomLeft">
+			<h2>Students' majors and jobs</h2>
+			<p>Two of these students have had jobs relating to their majors and one has had a job not relating to their major. Learn about their majors and jobs!</p>
+		</section>
+
+		<section class="bottomRight">
+			<a href="students.html">Read about these students!</a>
+		</section>
 		
-		<h2 class="Person1"> Aliye: Visual Media Specialist </h2>
-
-		<section class="ContentBlock" id="AliyeAbout">
-
-			<figure><img src="images/Aliye.jpg" alt="Aliye the photographer and her camera"></figure>
-
-			<h3>DIY Major</h3>
-			<p> I created my own major here at the U of R (a DIY major!), through the interdepartmental major track. I realized in my first year that none of the majors that were offered were exactly right for me, and started to look into the interdepartmental major option. Basically, you're petitioning the school that none of the majors would fulfil what you want to do.</p>
-
-			<h3>Building the Major</h3>
-			<p>My sophomore year, I began the process of designing my major, by having a lot of meetings with the Multidisciplinary Studies office, talking to many professors that I had taken classes with that I liked, and attempting to create a solid way of describing the major that I wanted to create.</p>
-
-		</section>
-
-		<section class="ContentBlock" id="AliyeMusic">
-
-			<figure><img src="images/Music.jpg" alt="Colorful music collage"></figure>
-
-			<h3>Discovering a Path</h3>
-			<p>This past summer was affirming that I’ve chosen the right path. I worked at a digital media and PR firm that works with classical musicians. I loved the experience, and worked on a lot of different projects for different artists. After graduation, I plan to pursue working on digital media for arts-based clients.</p>
-
-			<h3>The Job</h3>
-			<p> Here is a list of things that I have done job-wise: I added content (mostly concert dates) to websites, updated and checked up on Facebook pages, worked with photo and video content, updated PR logs, and helped to created digital media plans at the quarter.</p>
-
-		</section>
-
-		<section class="ContentBlock" id="AliyeFilm">
-
-			<figure><img src="images/Film.png" alt="Director's cut and a film reel"></figure>
-
-			<h3>Work in Film</h3>
-			<p>Last fall, I studied abroad in London, and I had an internship at a firm that interviews film stars and filmmakers for promo for films, and edits them into YouTube videos. I had the chance to interview stars, directors, and producers for a film festival in junket setting (sitting down in a room with the movie poster behind you), as well as twice on the red carpet.</p>
-
-		</section>
-
-		<section class="ContentBlock" id="AliyePhotography">
-
-			<figure><img src="images/Photo.jpg" alt="Close up of a camera lens"></figure>
-
-			<h3>Photographs</h3>
-			<p>I also love photography and the arts, and am a digital photographer, though I love working with film photography as well.</p>
-
-			<h3>Class Inspiration</h3>
-			<p> I took an Art History class about early photography, and we took frequent trips to the Eastman Museum as part of the course. I studied one of the nine remaining Daugerreotypes (the first form of permanent photography) of Fredrick Douglass, which was on loan at the Rare Books and Special Collections library at the time.</p>
-
-			<h3>The Future</h3>
-			<p> I hope at some point in my career, I’ll be able to combine my passion for photography and the arts and my work in Digital Media.</p>
-
-		</section>
-
-		<h2 class="Person2">Famous: Coding and Culture</h2>
-
-		<section class="ContentBlock" id="FamousAbout">
-
-			<figure><img src="images/Famous.jpg" alt="Famous and friends outside"></figure>
-
-			<h3>Hopes and Plans</h3>
-			<p>I could never state to know the entirety of my life's plan, however, since the onset of my involvement in undergraduate research, I can say with a degree of certainty that it would involve emerging media and historical preservation and education.</p>
-
-			<h3>Working with U of R</h3>
-			<p>I am computer science and digital media studies major. I work in two research labs. One in the ROC HCI lab in Wegmans under the guidance of Ehsan Hoque. I also work in the digial scholarschip lab in Rush Reeves library near the rare bare sections.</p>
-
-		</section>
-
-		<section class="ContentBlock" id="FamousRealization">
-
-			<figure><img src="images/History.jpg" alt="A Sillouhette standing on and looking at digital history images"></figure>
-
-			<h3>Making the Connection</h3>
-			<p>It happened one Spring ago as I sat studying in the dimly lit library stacks across from the Rare Books section at the University of Rochester in New York. As I sat programming next to works such as Porch's "The French Secret Services," Howard's "The German Empire," and isles of history and knowledge I've never experienced, I suddenly put it together: this was my approach to media and communication. Interactive history, digitization, a game. Presenting the wealth of past knowledge in ways future generations want to experience.</p>
-
-			<h3>From Here Forward</h3>
-			<p>The future, however, will not be ordained solely by code and technology. I realized it was imperative for me to explore actual literature and historical artifacts, so I began researching the University of Rochester's Rare Books department.</p>
-
-		</section>
-
-		<section class="ContentBlock" id="FamousHistory">
-
-			<figure><img src="images/Wireframe.png" alt="A digital wireframe model of an ancient man, child, and tools"></figure>
-
-			<h3>Digitizing History</h3>
-			<p>The Lazarus Lab, as well as the Digital Scholarship Lab in both in which I assist, have been looking into ways to bring rare artifacts out from behind velvet ropes and reinforced glass barriers. The project aims to digitize rare and fragile items to increase the range of audience they reach.</p>
-
-			<h3>Modeling History</h3>
-			<p>I came two laboratories that worked to craft three-dimensional representations busts, globals, and other sensitive items. Working with the Lazarus Lab and Digital Scholarship Lab brought me to a hybrid approach of Computer Science, Digital Media, and History that I never thought existed. In this approach, I use emerging technology to bring our past into the future and to breathe life and color into black ink on paper.</p>
-
-		</section>
-	</div>
-
-
-
-
-
-
-	<?php
-	  include('./inc/menu-highlighter.php');
-	  ?>
-
-</body>
+		<footer>
+			<div>Made by Kaleb Chitaphong</div>
+		</footer>
+	</body>
 
 </html>
--- oddStudent.php
+++ oddStudent.php
@@ -3,70 +3,105 @@
 <html lang="en">
 
 	<head>
+
 		<meta charset="UTF-8">
-				<?php
-	  include('./inc/title.php');
-	  ?>
-		<link rel="stylesheet" href="css/odd.css">
-		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
-		<script src="sss/sss.min.js"></script>
-		<link rel="stylesheet" href="sss/sss.css" type="text/css" media="all">
-		<script>jQuery(function($) {$('.slider').sss();});</script>
+		<title>Odd Student Out</title>
+		<link type="text/css" rel="stylesheet" href="css/studentPage.css">
 	</head>
 
 	<body>
-		<div class="grid">
-		<header>
 
-		<?php include('./inc/page-header.php');?>
-			<?php include('./inc/nav.php');?>
+		<section class="title">
+			<header>
 
-		</header>
+				<h1>Odd Student Out: 3 students and their majors and jobs</h1>
 
-		<div class="hero-image">
-			<div class="hero-text">
-				<h1>Political Science</h1>
-				<p>Study and Practice</p>
-			</div>
-		</div>
+			</header>
+		</section>
+
+		<section class="navigation">
+			<nav>
+				<ul>
+					<li>
+						<a href="index.html">Home</a>
+						<a href="students.html">Student Info</a>
+					</li>
+				<ul>
+		</section>
+		<main>
+			<section class="studentInfoBlock" id="similarStudents">
+				<h2>Two Similar Students</h2>
+
+				<div class="studentInfo">
+					<div class="titleBlock">
+						<h3>Brandon</h3>
+						<img src="images/brandon.jpg">
+					</div>
+					<div class="infoBlock">
+						<h4>Major</h4>
+						<p>First-generation student studying Financial Economics and Computer Science at the University of Rochester.</p>
+					</div>
+
+					<div class="infoBlock">
+						<h4>Jobs</h4>
+						<p>Financial Strategy: Managed the five year financial model for Artemis, an e-commerce jewelry company whose core mission is to empower refugees through education by redirecting their social entrepreneurial efforts at aiding Tibetan refugees. Turned the company profitable one year faster than previously projected and increased the amount of refugees aided by nearly 10%.</p>
+						<p>Technology and Computer Science: At the Simon Business School, I help offices in their transition to utilizing more technology based tools to make operations more efficient and I am a direct source of instant technological support.</p>
+					</div>
+				</div>
+
+				<div class="studentInfo">
+					<h3>Famous</h3>
+					<img src="images/famous.jpg">
+					<div class="infoBlock">
+						<h4>Major</h4>
+						<p>Hello Hello! My Name is the famous! I am computer science and digiatl media studies major.</p>
+					</div>
+
+					<div class="infoBlock">
+						<h4>Jobs</h4>
+						<p>I realized it was imperative for me to explore actual literature and historical artifacts, so I began researching the University of Rochester's Rare Books department. I came two laboratories that worked to craft three-dimensional representations busts, globals, and other sensitive items. Working with the Lazarus Lab and Digital Scholarship Lab brought me to a hybrid approach of Computer Science, Digital Media, and History that I never thought existed. In this approach, I use emerging technology to bring our past into the future and to breathe life and color into black ink on paper.</p>
+					</div>
+				</div>
+
+			</section>
+
+			<section class="studentInfoBlock" id="oddOneOut">
+				<h2>The Odd Student Out</h2>
+				<div class="studentInfo">
+					<h3>Kaleb</h3>
+					<img src="images/kaleb.jpg">
+					<div class="infoBlock">
+						<h4>Major</h4>
+						<p>I am a Political Science major, with a minor in CSC and a cluster in visual arts. My Political Science major has a focus on theoretical political science and the use of models in research.</p>
+					</div>
+
+					<div class="infoBlock">
+						<h4>Jobs</h4>
+						<p>The most noteworthy job I’ve had was as a canoe tripping guide for Camp Pathfinder, located in Algonquin Park Ontario. Camp Pathfinder acts as a host for kids from ages 7 to 15 looking to go on wilderness excursions. I spent the majority of my time out on canoe trips, however when needed I would act as a counselor. For my job as a guide, I was in charge of organizing, planning, and leading canoe trips for anywhere from two to fifteen days. A canoe trip consists of reaching different lakes each day by paddling and carrying the canoes.</p>
+					</div>
+				</div>
+			</section>
+		</main>
 
 		<aside>
-		<div class="slider">
-			<img src="images/Kaleb.jpg" alt="Kaleb" />
-			<img src="images/CityHall.jpeg"  alt="CityHall" />
-			<img src="images/Government.png" alt="Government" />
-			<img src="images/Books.jpg" alt="Books" />
-		</div>
+
+			<div class="asideBlock">
+				<h2>About Brandon's Hometown</h2>
+				<p>I am from a city close to Rochester, New York. Rochester is a city on Lake Ontario, in New York State. Old industrial buildings cluster near the Genesee River’s High Falls. A vast collection of toys and dolls forms the core of the Strong National Museum of Play.</p>
+			</div>
+
+			<div class="asideBlock">
+				<h2>Famous' story</h2>
+				<p>It happened one Spring ago as I sat studying in the dimly lit library stacks across from the Rare Books section at the University of Rochester in New York. I suddenly put it together: this was my approach to media and communication. Interactive history, digitization, a game. Presenting the wealth of past knowledge in ways future generations want to experience.</p>
+			</div>
+
+			<div class="asideBlock">
+				<h2>Kaleb's Future Plans</h2>
+				<p>For years I've wanted to work in government and public service in some way, hopefully in Rochester, my hometown.</p>
+			</div>
+
 		</aside>
-
-		<article>
-		<section class="ContentBlock" id="KalebAbout">
-
-			<h2>Hometown</h2>
-			<p>I am from Rochester New York and attended high school in the city of Rochester. I have lived within 30 minutes of the city of Rochester for my entire life.</p>
-
-		</section>
-
-		<section class="ContentBlock" id="KalebPoliSci">
-
-			<h2>Theoretical Politics</h2>
-			<p>My Political Science major has a focus on theoretical political science and the use of models in research. Using this discipline I plan on completing research in the field of voting institutions, outcomes, and logical choice in voting.</p>
-
-		</section>
-
-		<section class="ContentBlock" id="KalebCareer">
-
-			<h2>Working at Home</h2>
-			<p> For years I've wanted to work in government and public service in some way, hopefully in Rochester, my hometown.</p>
-
-		</section>
-		</article>
-
-
-	<?php
-	  include('./inc/menu-highlighter.php');
-	  ?>
-	</div>
+		
 	</body>
 
 </html>
